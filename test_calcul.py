9b2d7cc27f953736d485bd84bfb11eafdf8da324
mbejaoui80 <testautomationmbe@gmail.com>
2025-12-19 10:05:18 +0100

    Test passant

--- test_calcul.py
+++ test_calcul.py
@@ -1,7 +1,7 @@
 # Un test très simple pour notre robot CI/CD
 def test_addition():
     resultat = 1 + 1
-    assert resultat == 9
+    assert resultat == 2
     print("Test réussi : 1 + 1 fait bien 2 !")
 
 if __name__ == "__main__":
